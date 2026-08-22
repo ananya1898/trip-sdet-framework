0ee38e4e6f4c424d8ee1188bd92857107cef6dce
Ananya Agarwal <ananya.agr18@gmail.com>
2026-08-22 22:48:03 +0530

    Added logging to framework

--- framework/api/clients/base_client.py
+++ framework/api/clients/base_client.py
@@ -1,6 +1,9 @@
 #Generic HTTP behavior
 
 import requests
+from framework.utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 class BaseClient:
@@ -9,19 +12,31 @@ class BaseClient:
         self.base_url = base_url
 
     def get(self, endpoint):
-        return requests.get(self.base_url + endpoint)
+        logger.info(f"Sending GET request to {self.base_url + endpoint}")
+        response = requests.get(self.base_url + endpoint)
+        logger.info(f"GET request response: {response.status_code}")
+        return response
 
     def post(self, endpoint, json=None):
-        return requests.post(
+        logger.info(f"Sending POST request to {self.base_url + endpoint}")                  
+        response = requests.post(
             self.base_url + endpoint,
             json=json
         )
+        logger.info(f"POST request response: {response.status_code}")
+        return response
 
     def put(self, endpoint, json=None):
-        return requests.put(
+        logger.info(f"Sending PUT request to {self.base_url + endpoint}")
+        response = requests.put(
             self.base_url + endpoint,
             json=json
         )
+        logger.info(f"PUT request response: {response.status_code}")
+        return response
 
     def delete(self, endpoint):
-        return requests.delete(self.base_url + endpoint)
+        logger.info(f"Sending DELETE request to {self.base_url + endpoint}")
+        response = requests.delete(self.base_url + endpoint)
+        logger.info(f"DELETE request response: {response.status_code}")
+        return response
